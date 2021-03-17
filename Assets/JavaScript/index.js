7c8e6515f3f474c883246fe1c759fabb276cea83
rahul2-byte <rahulchand4299@gmail.com>
2021-03-17 10:39:10 +0530

    new

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -7,3 +7,4 @@ fetch("https://api.covid19api.com/summary", requestOptions)
   .then((response) => response.json())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
+let chart = document.getElementById("Corona-chart").getContext("2d");
